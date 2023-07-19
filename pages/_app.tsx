ae757011c29cfec8882438414a343d4d772b02ad
kevo-lianli <akerimberdi@gmail.com>
2023-07-19 20:41:43 +0300

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 // import localFont from "@next/font/local";
+import { Analytics } from "@vercel/analytics/react";
 
 import { Poppins } from "@next/font/google";
 
@@ -30,6 +31,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <main className={`${poppins.className} font-sans`}>
       <Component {...pageProps} />
+      <Analytics />
     </main>
   );
 }
