6ff6fbce73dd06b0534effe52b961b617af2a5cb
kevo-lianli <akerimberdi@gmail.com>
2023-09-14 19:24:56 +0300

    link update

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,18 +6,20 @@ import discord from "../public/images/discord-240.png";
 import telegram from "../public/images/telegram-240.png";
 const Header = () => {
   return (
-    <div className="w-full flex justify-between p-6 items-center absolute z-10">
-      <Image src={logo} alt="logo" width={120} />
-      <div className="flex space-x-5">
-        <a href="https://twitter.com/EcoLandWorld" target={"_blank"}>
-          <Image src={twitter} alt="twitter" width={32} />
-        </a>
-        <a href="https://discord.gg/whMCaCSURu" target={"_blank"}>
-          <Image src={discord} alt="discord" width={32} />
-        </a>
-        <a href="https://t.me/ecoland_news" target={"_blank"}>
-          <Image src={telegram} alt="telegram" width={32} />
-        </a>
+    <div className="flex items-center justify-center">
+      <div className="container flex p-6 mt-20 justify-between items-center absolute z-10">
+        <Image src={logo} alt="logo" width={120} />
+        <div className="flex space-x-5">
+          <a href="https://twitter.com/EcoLandWorld" target={"_blank"}>
+            <Image src={twitter} alt="twitter" width={32} />
+          </a>
+          <a href="https://discord.gg/ckffrhrywk" target={"_blank"}>
+            <Image src={discord} alt="discord" width={32} />
+          </a>
+          <a href="https://t.me/ecoland_news" target={"_blank"}>
+            <Image src={telegram} alt="telegram" width={32} />
+          </a>
+        </div>
       </div>
     </div>
   );
